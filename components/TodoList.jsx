cc1ed13da25bdb8fb212d000faec68f43b9d62d2
gharatkaushal05 <gharatkaushal05@gmail.com>
2024-06-26 00:43:48 +0530

    first init

---
+++ components/TodoList.jsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import RemoveBtn from "./RemoveBtn";
+import { HiPencilAlt } from "react-icons/hi";
+
+const getTodo = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch todo");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading todo: ", error);
+  }
+};
+
+export default async function TodoList() {
+  const { todo } = await getTodo();
+
+  return (
+    <>
+      {todo.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
+
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/editTodo/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
