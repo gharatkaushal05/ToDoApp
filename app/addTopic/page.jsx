12ba72b341b9e12b90bfe5f09ed1b8588695085a
gharatkaushal05 <gharatkaushal05@gmail.com>
2024-06-27 13:34:04 +0530

    ui updated

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -58,7 +58,7 @@ export default function AddTopic() {
         type="submit"
         className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
       >
-        Add Topic
+        Add Todo
       </button>
     </form>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center bg-gray-900 px-8 py-3 shadow-md">
-      <Link className="text-white font-bold text-xl" href={"/"}>
+    <nav className="flex justify-around flex-col items-center bg-orange-400 px-8 py-3 h-[150px] rounded-lg shadow-md">
+      <Link className="text-white font-bold text-3xl" href={"/"}>
         Todo App
       </Link>
       <Link className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105" href={"/addTopic"}>
